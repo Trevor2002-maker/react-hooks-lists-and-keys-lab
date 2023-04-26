bee06ca26f4fdf974ef5f93653f96d1fde799eba
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-04-26 15:25:00 +0300

    DOne

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -7,7 +7,7 @@ function ProjectList({ projects }) {
     <div id="projects">
       <h2>My Projects</h2>
       <div id="project-list">{projects.map(project =>(
-        <ProjectItem name={project.name} about={project.about} technologies={project.technologies} />
+        <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies} />
       ))}</div>
     </div>
   );
